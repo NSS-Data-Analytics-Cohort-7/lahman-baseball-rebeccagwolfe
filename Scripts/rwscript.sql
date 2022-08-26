9eb50d368015f5859f51833ed889712fecb19515
rebeccagwolfe <rebeccagwolfe@gmail.com>
2022-08-25 19:53:43 -0500

    working on 4

--- Scripts/rwscript.sql
+++ Scripts/rwscript.sql
@@ -1,6 +1,13 @@
 -- 1. What range of years for baseball games played does the provided database cover? 
 1871 through 2016
 
+SELECT debut
+FROM people;
+
+
+FROM teams;
+
+
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
 
 SELECT playerid, batting.teamid, teams.name
@@ -45,21 +52,48 @@ ORDER BY height
    
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each playerâ€™s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
-SELECT p.namefirst, p.namelast, t.teamid, t.name
+SELECT p.namefirst, p.namelast, s.schoolname, CAST(SUM(DISTINCT(CAST(salary AS NUMERIC)))AS MONEY) AS total_salary
 FROM people AS p
-INNER JOIN appearances AS a
+INNER JOIN salaries as sa
 USING (playerid)
-INNER JOIN teams AS t
-ON t.teamid = a.teamid
-ORDER BY t.name DESC;
+INNER JOIN collegeplaying AS c
+USING (playerid)
+INNER JOIN schools AS s
+USING (schoolid)
+WHERE s.schoolname = 'Vanderbilt University' 
+GROUP BY p.namefirst, p.namelast, s.schoolname
+ORDER BY total_salary DESC;
 
-SELECT name
-FROM teams
-ORDER BY name DESC;
+--SHows the list of vanderbilt players with their salaries using distinct
 
--- Vanderbilt University not listed in teams table. Schools, duh. 
+SELECT p.namefirst, p.namelast, s.schoolname, CAST(SUM(CAST(salary AS NUMERIC))AS MONEY) AS total_salary
+FROM people AS p
+INNER JOIN salaries as sa
+USING (playerid)
+INNER JOIN collegeplaying AS c
+USING (playerid)
+INNER JOIN schools AS s
+USING (schoolid)
+WHERE s.schoolname = 'Vanderbilt University' AND namefirst = 'David'
+GROUP BY p.namefirst, p.namelast, s.schoolname
+ORDER BY total_salary DESC;
 
-SELECT p.namefirst, p.namelast, s.schoolname, sa.salary
+-- David Price at $245,553,888
+
+SELECT p.namefirst, p.namelast, s.schoolname, CAST(CAST(salary AS NUMERIC)AS MONEY) AS total_salary
+FROM people AS p
+INNER JOIN salaries as sa
+USING (playerid)
+INNER JOIN collegeplaying AS c
+USING (playerid)
+INNER JOIN schools AS s
+USING (schoolid)
+WHERE s.schoolname = 'Vanderbilt University' AND namefirst = 'David'
+ORDER BY total_salary DESC;
+
+--Shows all the salaries that David earned. I noticed that each salary is listed three times...so then added distinc.
+
+SELECT p.namefirst, p.namelast, s.schoolname, CAST(SUM(DISTINCT(CAST(salary AS NUMERIC)))AS MONEY) AS total_salary
 FROM people AS p
 INNER JOIN salaries as sa
 USING (playerid)
@@ -68,9 +102,29 @@ USING (playerid)
 INNER JOIN schools AS s
 USING (schoolid)
 WHERE s.schoolname = 'Vanderbilt University'
-ORDER BY sa.salary DESC;
+GROUP BY p.namefirst, p.namelast, s.schoolname
+ORDER BY total_salary DESC;
+
+-- "David"	"Price"	"Vanderbilt University"	81851296
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+
+
+(SELECT COUNT(playerid) AS players_per_position,
+    CASE WHEN pos = 'OF' THEN 'Outfield'
+    WHEN pos = 'SS' THEN 'Infield'
+    WHEN pos = '1B' THEN 'Outfield'
+    WHEN pos = '2B' THEN 'Outfield'
+    WHEN pos = '3B' THEN 'Outfield'
+    WHEN pos = 'P' THEN 'Battery'
+    WHEN pos = 'C' THEN 'Battery'
+    ELSE 'NULL' END AS position
+FROM fielding
+    GROUP BY position)
+   
+SELECT PO
+FROM fielding
+WHERE fielding.yearid = 2016;
 
 SELECT COUNT(playerid) AS players_per_position,
     CASE WHEN pos = 'OF' THEN 'Outfield'
@@ -84,7 +138,7 @@ SELECT COUNT(playerid) AS players_per_position,
 FROM fielding
     GROUP BY position
     
-    SELECT COUNT(playerid) AS players_per_position, PO
+    SELECT COUNT(playerid) AS players_per_position, po,
     CASE WHEN pos = 'OF' THEN 'Outfield'
     WHEN pos = 'SS' THEN 'Infield'
     WHEN pos = '1B' THEN 'Outfield'
@@ -94,8 +148,34 @@ FROM fielding
     WHEN pos = 'C' THEN 'Battery'
     ELSE 'NULL' END AS position
 FROM fielding
-    GROUP BY position, PO
+    GROUP BY position, po
+  
     
     
     
+   
+-- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
+
+-- JOIN
+-- ROUND
+-- Need tables that has strikeouts , year, game, decade, and home runs (hr)
+
+-- Strikeouts (SO) is in pitching table
+--homeruns (hr) is in batting table
+
+SELECT COUNT(b.hr), COUNT(p.so), b.yearid
+FROM batting AS b
+INNER JOIN pitching AS p
+USING (playerid)
+GROUP BY p.so, b.yearid;
+
+SELECT so
+FROM pitching;
+
+SELECT AVG(strikeouts), game, decade
+FROM 
+GROUP BY game, decade
+WHERE year > 1920
+
+Does since 1920 include 1920? Or should it start with 1921?
 
